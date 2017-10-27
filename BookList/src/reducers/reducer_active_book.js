53ae639c0b198bea2e4a124faa4372cbabc6395f
QingpingMeng <missing1989@gmail.com>
2017-10-26 21:56:32 -0700

    add back

---
+++ BookList/src/reducers/reducer_active_book.js
@@ -0,0 +1,10 @@
+// State is not app state, only the state 
+// this deducer is responsable for 
+export default function (state = null, action) {
+    switch (action.type) {
+        case 'BOOK_SELECTED':
+            return action.payload;
+    }
+
+    return state
+}
